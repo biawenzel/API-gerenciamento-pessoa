15627db67feb06ad793c71eb4c38071c28bc6e64
biawenzel <biahwenzel@gmail.com>
2023-01-08 15:30:13 -0300

    PutMapping (id) and GetMappig (id)

--- src/main/java/com/pessoa/API/controller/PessoaController.java
+++ src/main/java/com/pessoa/API/controller/PessoaController.java
@@ -5,6 +5,7 @@ import com.pessoa.API.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -26,10 +27,22 @@ public class PessoaController {
         return pessoaRepository.save(pessoa);
     }
 
-    @PutMapping //editar uma pessoa
-    public Pessoa editar(Pessoa pessoa) {
-        return null;
+    @PutMapping("/{id}")
+    public Pessoa editar(@PathVariable long id, @RequestBody Pessoa pessoa) {
+        var pesquisaPessoa = pessoaRepository.findById(id);
+        if (pesquisaPessoa == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        pessoa.setId(id);
+        return pessoaRepository.save(pessoa);
     }
 
-    //Consultar uma pessoa
+    @GetMapping("/{id}")
+    public Pessoa consultar(@PathVariable long id) {
+        var pesquisaPessoa = pessoaRepository.findById(id);
+        if (pesquisaPessoa == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return pessoaRepository.findById(id);
+    }
 }
